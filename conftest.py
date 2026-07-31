d8ef357abfe0e1bf0c75c6ecf07958d87b5927bc
evgeny104 <nazawka88@gmail.com>
2026-07-31 16:53:29 +0300

    add test_search_homepage

--- conftest.py
+++ conftest.py
@@ -2,6 +2,14 @@ import pytest
 from playwright.sync_api import Page
 from pages.home_page import HomePage
 
+@pytest.fixture(scope="session")
+def browser_context_args(browser_context_args):
+    """Фиксируем английскую локаль, чтобы сайт не редиректил на другой язык"""
+    return {
+        **browser_context_args,
+        "locale": "en-US",
+    }
+
 @pytest.fixture(autouse=True)
 def home(page: Page) -> HomePage:
     """Фикстура стартовая страница"""
@@ -10,7 +18,7 @@ def home(page: Page) -> HomePage:
 @pytest.fixture(autouse=True)
 def open_metatrader(page: Page):
     """Фикстура для подготовки окружения теста"""
-    page.goto("https://www.metatrader.com/en")
+    page.goto("https://www.metatrader.com/")
     yield page
 
 
--- tests/test_page_nvda.py
+++ tests/test_page_nvda.py
@@ -1,18 +1,33 @@
 from playwright.sync_api import Page, expect
 from data.data_tests import EXPECTED_YEARS
+import pytest
 
 def test_search_nvda(page: Page):
     """
-    Проверяет поиск тикера NVDA через строку поиска на сайте MetaTrader:
+    Проверяет поиск NVDA через строку поиска на сайте MetaTrader:
     - в выпадающем списке подсказок появляется пункт с NVIDIA неточной формулировке
     - клик по подсказке ведет на страницу актива /symbols/nasdaq/nvda
     """
     page.get_by_role("searchbox").fill("NVDA")
-    expect(page.locator('a[href$="/symbols/nasdaq/nvda"]').first).to_contain_text("nvidia", ignore_case=True)
+    expect(page.locator('a[href$="/symbols/nasdaq/nvda"]').first).to_contain_text("Nvidia", ignore_case=True)
 
     # first in search
     page.locator('a[href$="/symbols/nasdaq/nvda"]').first.click()
     expect(page).to_have_url("https://www.metatrader.com/en/symbols/nasdaq/nvda")
+
+def test_search_homepage(page: Page):
+    """
+    Проверяет поиск через строку поиска на сайте MetaTrader:
+    """
+    expect(page).to_have_url("https://www.metatrader.com/")
+
+    search = page.get_by_role("searchbox")
+    search.fill("NVDA")
+    page.wait_for_timeout(1500)
+    search.press("Enter")
+
+    expect(page.get_by_text("NVDA Bullish Continuation Above Resistance")).to_be_visible()
+
 
 def test_year_columns_present(page: Page, open_nvda_statement):
     header = page.locator("span.row.header")
