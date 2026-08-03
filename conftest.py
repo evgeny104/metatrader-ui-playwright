656f5c4699e90b39b55e494f8c6d1548da07a117
evgeny104 <nazawka88@gmail.com>
2026-08-03 20:09:25 +0300

    update fixsture browser_type_launch_args add new param slow_mo

--- conftest.py
+++ conftest.py
@@ -2,29 +2,35 @@ import pytest
 from playwright.sync_api import Page
 from pages.home_page import HomePage
 
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
+    return {
+        **browser_type_launch_args,
+        "slow_mo": 500,  # замедление как --slowmo 500
+    }
+
+
 @pytest.fixture(scope="session")
 def browser_context_args(browser_context_args):
-    """Фиксируем английскую локаль, чтобы сайт не редиректил на другой язык"""
     return {
         **browser_context_args,
         "locale": "en-US",
     }
 
+
 @pytest.fixture(autouse=True)
 def home(page: Page) -> HomePage:
-    """Фикстура стартовая страница"""
     return HomePage(page)
+
 
 @pytest.fixture(autouse=True)
 def open_metatrader(page: Page):
-    """Фикстура для подготовки окружения теста"""
     page.goto("https://www.metatrader.com/")
     yield page
 
 
-
 @pytest.fixture()
 def open_nvda_statement(page: Page):
-    """Фикстура для подготовки окружения теста /nvda/statement"""
     page.goto("https://www.metatrader.com/en/symbols/nasdaq/nvda/statement")
     yield page
