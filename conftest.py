e219185c4e1d18eea236a370717617d54c595a9d
evgeny104 <nazawka88@gmail.com>
2026-08-06 19:11:17 +0300

    conftest: slow_mo 500 → 1000

--- conftest.py
+++ conftest.py
@@ -3,12 +3,17 @@ from playwright.sync_api import Page
 from pages.home_page import HomePage
 
 
-@pytest.fixture(scope="session")
-def browser_type_launch_args(browser_type_launch_args):
-    return {
-        **browser_type_launch_args,
-        "slow_mo": 500,  # замедление как --slowmo 500
-    }
+@pytest.fixture(autouse=True)
+def open_metatrader(page: Page):
+    page.goto("https://www.metatrader.com/")
+    yield page
+
+
+@pytest.fixture(autouse=True)
+def home(page: Page) -> HomePage:
+    return HomePage(page)
+
+
 
 
 @pytest.fixture(scope="session")
@@ -18,19 +23,14 @@ def browser_context_args(browser_context_args):
         "locale": "en-US",
     }
 
-
-@pytest.fixture(autouse=True)
-def home(page: Page) -> HomePage:
-    return HomePage(page)
-
-
-@pytest.fixture(autouse=True)
-def open_metatrader(page: Page):
-    page.goto("https://www.metatrader.com/")
-    yield page
-
-
 @pytest.fixture()
 def open_nvda_statement(page: Page):
     page.goto("https://www.metatrader.com/en/symbols/nasdaq/nvda/statement")
     yield page
+
+@pytest.fixture(scope="session")
+def browser_type_launch_args(browser_type_launch_args):
+    return {
+        **browser_type_launch_args,
+        "slow_mo": 1000,  # замедление как --slowmo 1000mc
+    }
