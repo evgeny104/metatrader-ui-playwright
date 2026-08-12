31f7e9b388258deefbbcf7a4f4575186d250176e
evgeny104 <nazawka88@gmail.com>
2026-08-13 00:19:45 +0300

    test optimization

--- tests/nvda/test_search_nvda.py
+++ tests/nvda/test_search_nvda.py
@@ -4,17 +4,21 @@ from pages.nvda.nvda_symbol_page import NvdaSymbolPage
 
 def test_search_via_dropdown(home: HomePage, nvda_page: NvdaSymbolPage):
     """Ввод 'NVDA' -> клик по первому пункту дропдауна -> страница NVDA"""
-    home.search("NVDA")
+    query  = "NVDA"
+    home.search(query)
     nvda_page.should_be_opened()
+
 
 def test_search_lowercase(home: HomePage, nvda_page: NvdaSymbolPage):
     """Поиск в нижнем регистре 'nvda' тоже открывает страницу NVDA"""
-    home.search("nvda")
+    query = "NVDA"
+    home.search(query)
     nvda_page.should_be_opened()
 
+
 def test_search_via_enter(home: HomePage, nvda_page: NvdaSymbolPage):
-    """Ввод 'NVIDIA' + Enter -> страница результатов -> клик по первому результату -> страница NVDA"""
-    home.search("NVIDIA", submit_with_enter=True) # вводит + жмёт Enter
-    home.open_first_result()                            # кликает первую ссылку в результатах
-    nvda_page.should_be_opened()                        # проверяет что попали на NVDA
+    """Ввод 'NVIDIA' + Enter -> клик по первому результату -> страница NVDA"""
+    query = "NVIDIA"
+    home.search(query, submit_with_enter=True)
+    home.open_first_result(query)
 
